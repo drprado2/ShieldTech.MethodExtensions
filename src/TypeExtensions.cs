0fb00adaeffaf1cf47e747180b5b2a5f32ff2f32
Adriano Oliveira <drprado2@gmail.com>
2020-06-14 10:39:27 -0300

    Criação de método de extensão para obter instancia padrão partindo de um Type.

---
+++ src/TypeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ShieldTech.MethodExtensions
+{
+    public static class TypeExtensions
+    {
+        public static object GetDefaultInstance(Type type)
+        {
+            if(type.IsValueType)
+            {
+                return Activator.CreateInstance(type);
+            }
+            return null;
+        }
+    }
+}
